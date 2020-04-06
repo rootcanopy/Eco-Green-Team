eaef2573f2d7a96b78936ed9aa260c75e43c8ee3
rootcanopy <dave.trees201@gmail.com>
2020-04-06 10:08:49 +0000

    second commit

---
+++ assets/JavaScript/eco.js
@@ -0,0 +1,12 @@
+
+
+$(document).ready(function () {
+    $("#one-answer").hide();
+
+    $("#good-one").click(function () {
+        $("#one-answer").show();
+    });
+    $("#bad-one").click(function () {
+        $("#one-answer").show();
+    });
+});
